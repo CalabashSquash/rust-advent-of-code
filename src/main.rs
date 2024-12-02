b8c3e33b2561b72bcdbd74b7e5e5a8caf2e3d277
CalabashSquash <my_name_uhh@protonmail.com>
2024-12-03 08:10:36 +1100

    Day 2

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+mod day1;
+mod day2;
+use crate::day1::{problem1, problem2};
+use crate::day2::{problem1 as d2p1};
+
+fn main() {
+    println!("Hello, world!");
+    // println!("Day 1 problem 1: {}", problem1());
+    // println!("Day 1 problem 2: {}", problem2());
+    println!("Day 2 problem 2: {}", d2p1());
+}
