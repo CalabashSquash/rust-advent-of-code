16849c424c42418cbcfd70086da3a1c1b39c954b
CalabashSquash <my_name_uhh@protonmail.com>
2024-12-14 16:47:33 +1100

    Day 13

--- 2024/day-13/src/part1.rs
+++ 2024/day-13/src/part1.rs
@@ -1,5 +1,12 @@
-use nom::{bytes::complete::tag, character::complete::{self, line_ending, multispace0}, combinator::opt, multi::many0, sequence::terminated, IResult};
 use miette::miette;
+use nom::{
+    bytes::complete::tag,
+    character::complete::{self, line_ending, multispace0},
+    combinator::opt,
+    multi::many0,
+    sequence::terminated,
+    IResult,
+};
 
 #[derive(Debug)]
 struct Coordinates {
@@ -11,31 +18,61 @@ struct Coordinates {
 struct Machine {
     button_a: Coordinates,
     button_b: Coordinates,
-    prize: Coordinates
+    prize: Coordinates,
 }
 
 // Brute force solution
 #[tracing::instrument]
 pub fn process(_input: &str) -> miette::Result<String> {
-    let (remaining, machines) = parse(_input).map_err(|e| miette!("Error parsing: {}", e))?;
-    println!("remaining: {}",remaining);
-    println!("machines: {:#?}",machines);
+    let (_, machines) = parse(_input).map_err(|e| miette!("Error parsing: {}", e))?;
 
-    
-    todo!("day 01 - part 1");
+    let result: u32 = machines
+        .iter()
+        .map(|machine| find_cheapest(machine))
+        .flatten()
+        .into_iter()
+        .sum();
+    Ok(result.to_string())
+}
+
+fn find_cheapest(machine: &Machine) -> Option<u32> {
+    let mut min: u32 = std::u32::MAX;
+    for a in 0..1000 {
+        for b in 0..1000 {
+            let x = a * machine.button_a.x + b * machine.button_b.x;
+            let y = a * machine.button_a.y + b * machine.button_b.y;
+            if x == machine.prize.x && y == machine.prize.y {
+                // Found a valid solution
+                let cost = 3 * a + b;
+                if cost < min {
+                    min = cost;
+                }
+            }
+        }
+    }
+    return if min == std::u32::MAX {
+        None
+    } else {
+        Some(min)
+    };
 }
 
 fn parse(_input: &str) -> IResult<&str, Vec<Machine>> {
     many0(parse_machine)(_input)
 }
 
-
 fn parse_x_y(_input: &str) -> IResult<&str, Coordinates> {
     let (remaining, _) = tag("X+")(_input)?;
     let (remaining, x_dist) = complete::u32(remaining)?;
     let (remaining, _) = tag(", Y+")(remaining)?;
     let (remaining, y_dist) = complete::u32(remaining)?;
-    Ok((remaining, Coordinates {x: x_dist, y: y_dist}))
+    Ok((
+        remaining,
+        Coordinates {
+            x: x_dist,
+            y: y_dist,
+        },
+    ))
 }
 
 fn parse_prize(_input: &str) -> IResult<&str, Coordinates> {
@@ -43,7 +80,7 @@ fn parse_prize(_input: &str) -> IResult<&str, Coordinates> {
     let (remaining, x) = complete::u32(remaining)?;
     let (remaining, _) = tag(", Y=")(remaining)?;
     let (remaining, y) = complete::u32(remaining)?;
-    Ok((remaining, Coordinates {x, y}))
+    Ok((remaining, Coordinates { x, y }))
 }
 
 fn parse_machine(_input: &str) -> IResult<&str, Machine> {
@@ -54,7 +91,14 @@ fn parse_machine(_input: &str) -> IResult<&str, Machine> {
     let (remaining, prize) = terminated(parse_prize, opt(line_ending))(remaining)?;
     let (remaining, _) = multispace0(remaining)?;
 
-    Ok((remaining, Machine {button_a, button_b, prize}))
+    Ok((
+        remaining,
+        Machine {
+            button_a,
+            button_b,
+            prize,
+        },
+    ))
 }
 
 #[cfg(test)]
@@ -69,8 +113,16 @@ Prize: X=8400, Y=5400
 
 Button A: X+26, Y+66
 Button B: X+67, Y+21
-Prize: X=12748, Y=12176";
-        assert_eq!("", process(input)?);
+Prize: X=12748, Y=12176
+
+Button A: X+17, Y+86
+Button B: X+84, Y+37
+Prize: X=7870, Y=6450
+
+Button A: X+69, Y+23
+Button B: X+27, Y+71
+Prize: X=18641, Y=10279";
+        assert_eq!("480", process(input)?);
         Ok(())
     }
 }
--- 2024/day-13/src/part2.rs
+++ 2024/day-13/src/part2.rs
@@ -1,6 +1,117 @@
+use miette::miette;
+use nom::{
+    bytes::complete::tag,
+    character::complete::{self, line_ending, multispace0},
+    combinator::opt,
+    multi::many0,
+    sequence::terminated,
+    IResult,
+};
+
+#[derive(Debug)]
+struct Coordinates {
+    x: i64,
+    y: i64,
+}
+
+#[derive(Debug)]
+struct Machine {
+    button_a: Coordinates,
+    button_b: Coordinates,
+    prize: Coordinates,
+}
+
+// Brute force solution
 #[tracing::instrument]
 pub fn process(_input: &str) -> miette::Result<String> {
-    todo!("day 01 - part 2");
+    let (_, machines) = parse(_input).map_err(|e| miette!("Error parsing: {}", e))?;
+
+    let result: i64 = machines
+        .iter()
+        .map(|machine| find_cheapest(machine))
+        .flatten()
+        .sum();
+    Ok(result.to_string())
+}
+
+fn find_cheapest(machine: &Machine) -> Option<i64> {
+    // X: a*machine.button_a.x + b*machine.button_b.x = machine.prize.x
+    // Y: a*machine.button_a.y + b*machine.button_b.y = machine.prize.y
+    //1X: (a*machine.button_a.x * machine.button_b.y) + (b*machine.button_b.x * machine.button_b.y) = machine.prize.x * machine.button_b.y
+    //1Y: (a*machine.button_a.y * machine.button_b.x) + (b*machine.button_b.y * machine.button_b.x) = machine.prize.y * machine.button_b.x
+
+    //1D: (a*machine.button_a.x * machine.button_b.y) - (a*machine.button_a.y * machine.button_b.x) = (machine.prize.x * machine.button_b.y) - (machine.prize.y * machine.button_b.x)
+    //2D: a*(machine.button_a.x*machine.button_b.y - machine.button_a.y*machine.button_b.x) = (machine.prize.x * machine.button_b.y) - (machine.prize.y * machine.button_b.x)
+    //3D: a == divide right side by (machine.button_a.x*machine.button_b.y - machine.button_a.y*machine.button_b.x)
+    //    If not a whole number (x%y!=0), no solution.
+
+    let x_a_coeff = machine.button_a.x * machine.button_b.y;
+    // let x_b_coeff = machine.button_b.x * machine.button_b.y;
+    let x_solution =   machine.prize.x * machine.button_b.y;
+
+    let y_a_coeff = machine.button_a.y * machine.button_b.x;
+    // let y_b_coeff = machine.button_b.y * machine.button_b.x;
+    let y_solution =   machine.prize.y * machine.button_b.x;
+
+    let diff_a_coeff = x_a_coeff - y_a_coeff;
+    let diff_solution = x_solution - y_solution;
+    let a = diff_solution / diff_a_coeff;
+    if diff_solution % diff_a_coeff != 0 {
+        return None;
+    }
+
+    //e.g. 22b = 8400 - 94a
+    let b_times_bx = machine.prize.x - a * machine.button_a.x;
+    let b = b_times_bx / machine.button_b.x;
+    if b_times_bx % machine.button_b.x != 0 {
+        return None
+    }
+
+    Some(3 * a + b)
+}
+
+fn parse(_input: &str) -> IResult<&str, Vec<Machine>> {
+    many0(parse_machine)(_input)
+}
+
+fn parse_x_y(_input: &str) -> IResult<&str, Coordinates> {
+    let (remaining, _) = tag("X+")(_input)?;
+    let (remaining, x_dist) = complete::i64(remaining)?;
+    let (remaining, _) = tag(", Y+")(remaining)?;
+    let (remaining, y_dist) = complete::i64(remaining)?;
+    Ok((
+        remaining,
+        Coordinates {
+            x: x_dist,
+            y: y_dist,
+        },
+    ))
+}
+
+fn parse_prize(_input: &str) -> IResult<&str, Coordinates> {
+    let (remaining, _) = tag("Prize: X=")(_input)?;
+    let (remaining, x) = complete::i64(remaining)?;
+    let (remaining, _) = tag(", Y=")(remaining)?;
+    let (remaining, y) = complete::i64(remaining)?;
+    Ok((remaining, Coordinates { x: x + 10000000000000, y: y + 10000000000000 }))
+}
+
+fn parse_machine(_input: &str) -> IResult<&str, Machine> {
+    let (remaining, _) = tag("Button A: ")(_input)?;
+    let (remaining, button_a) = terminated(parse_x_y, line_ending)(remaining)?;
+    let (remaining, _) = tag("Button B: ")(remaining)?;
+    let (remaining, button_b) = terminated(parse_x_y, line_ending)(remaining)?;
+    let (remaining, prize) = terminated(parse_prize, opt(line_ending))(remaining)?;
+    let (remaining, _) = multispace0(remaining)?;
+
+    Ok((
+        remaining,
+        Machine {
+            button_a,
+            button_b,
+            prize,
+        },
+    ))
 }
 
 #[cfg(test)]
@@ -9,8 +120,21 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
+        let input = "Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=8400, Y=5400
+
+Button A: X+26, Y+66
+Button B: X+67, Y+21
+Prize: X=12748, Y=12176
+
+Button A: X+17, Y+86
+Button B: X+84, Y+37
+Prize: X=7870, Y=6450
+
+Button A: X+69, Y+23
+Button B: X+27, Y+71
+Prize: X=18641, Y=10279";
         assert_eq!("", process(input)?);
         Ok(())
     }
